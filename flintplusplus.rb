303c94498d4a798be0df4a576337995224ec60b3
sawada_yasuharu <sawada_yasuharu@cyberagent.co.jp>
2015-07-30 09:56:39 +0900

    Renamed

--- flintplusplus.rb
+++ flintplusplus.rb
@@ -1,17 +1,15 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+require "formula"
 
 class Flintplusplus < Formula
   desc "Flint++ is cross-platform, zero-dependency port of flint, a lint program for C++ developed and used at Facebook."
   homepage "http://l2program.co.uk/category/flint"
   url "https://github.com/L2Program/FlintPlusPlus/archive/master.zip"
   sha256 "7f48a52ca50a3ba5e64c7440e9e5876a0bd624726456896c9f6993a99fec4fff"
+  version "0.0"
 
   def install
-    system "cd flint"
-    system "make" # if this fails, try separate make/make install steps
-    bin.install "flint++"
+    man1.install "flint++.1"
+    system "cd flint; make"
+    bin.install "flint/flint++"
   end
-
 end
